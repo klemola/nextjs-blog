84df466a228d57779a37cf6050a5e6da8675266c
Matias Klemola <matias.klemola@gmail.com>
2020-09-08 16:02:39 +0300

    Convert the codebase to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
+import { GetStaticProps } from "next"
 import Head from "next/head"
 import Link from "next/link"
 
-import Date from "../components/date"
 import Layout, { siteTitle } from "../components/layout"
 import utilStyles from "../styles/utils.module.css"
 import { getSortedPostsData } from "../lib/posts"
+import Date from "../components/date"
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -21,7 +30,7 @@ export default function Home({ allPostsData }) {
           </a>
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you’ll be building a site like this in{" "}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
       </section>
@@ -45,7 +54,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
